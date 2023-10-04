1dedca472a3a0012079f8ecc7911e09a206a6b0a
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-10-04 11:01:06 +0100

    db ssl debug

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,13 +1,17 @@
 import { Pool } from "pg";
 
-const environmentVariableDB_SSL = Boolean(process.env.DB_SSL);
+// Debug
+console.log("DEBUG----------");
+console.log("process.env.DB_SSL:", process.env.DB_SSL);
+console.log("typeof process.env.DB_SSL:", typeof process.env.DB_SSL);
 
 export const database = new Pool({
   connectionString: process.env.DB_CONNECTION_STRING,
   // This property controls whether or not to reject connections that are made to servers with self-signed or invalid SSL certificates.
   // Set it to false to disable SSL/TLS verification.
   // Enable when Hosted / Disable when Dev/Testing
-  ssl: { rejectUnauthorized: environmentVariableDB_SSL },
+  // ssl: { rejectUnauthorized: environmentVariableDB_SSL },
+  ssl: process.env.DB_SSL === "true" ? true : false,
 });
 
 database.connect((error, client) => {
