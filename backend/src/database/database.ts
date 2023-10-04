ffa18b184a94668bb4729f4e7d2c2d7ae5859a85
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-10-04 11:03:46 +0100

    db ssl debug

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,17 +1,10 @@
 import { Pool } from "pg";
 
-// Debug
-console.log("DEBUG----------");
-console.log("process.env.DB_SSL:", process.env.DB_SSL);
-console.log("typeof process.env.DB_SSL:", typeof process.env.DB_SSL);
-
 export const database = new Pool({
   connectionString: process.env.DB_CONNECTION_STRING,
-  // This property controls whether or not to reject connections that are made to servers with self-signed or invalid SSL certificates.
-  // Set it to false to disable SSL/TLS verification.
+  // https://node-postgres.com/features/ssl
   // Enable when Hosted / Disable when Dev/Testing
-  // ssl: { rejectUnauthorized: environmentVariableDB_SSL },
-  ssl: process.env.DB_SSL === "true" ? true : false,
+  ssl: Boolean(process.env.DB_SSL),
 });
 
 database.connect((error, client) => {
