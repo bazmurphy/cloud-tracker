7398ac660e63000071ba7d7c2949819b1ef4c5f6
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-10-04 11:06:55 +0100

    db ssl debug

--- backend/src/database/database.ts
+++ backend/src/database/database.ts
@@ -1,10 +1,15 @@
 import { Pool } from "pg";
 
+// Debug
+console.log("DEBUG----------");
+console.log("process.env.DB_SSL:", process.env.DB_SSL);
+console.log("typeof process.env.DB_SSL:", typeof process.env.DB_SSL);
+
 export const database = new Pool({
   connectionString: process.env.DB_CONNECTION_STRING,
   // https://node-postgres.com/features/ssl
-  // Enable when Hosted / Disable when Dev/Testing
-  ssl: Boolean(process.env.DB_SSL),
+  // Enable when Hosted / Disable when Local/Development/Testing
+  ssl: process.env.DB_SSL === "true" ? true : false,
 });
 
 database.connect((error, client) => {
