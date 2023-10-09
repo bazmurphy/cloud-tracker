57fb9331af6f3f54d23d49b259d581135205c3dd
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-10-09 15:33:46 +0100

    adjusted backend workflow

--- .github/workflows/deploy-backend.sh
+++ .github/workflows/deploy-backend.sh
@@ -2,9 +2,9 @@
 DB_CONNECTION_STRING="$1"
 DB_SSL="$2"
 echo "Attempting Deploy..." &&
-pm2 list | grep -q "cloudtrackerapi" && pm2 delete all || true &&
-rm -rf cloudtracker &&
-cp -r backend cloudtracker &&
+pm2 list | grep -q "cloud-tracker-api" && pm2 delete all || true &&
+rm -rf cloud-tracker &&
+cp -r backend cloud-tracker &&
 cd backend &&
-pm2 start "cloudtracker/backend/dist/index.js" --name cloudtrackerapi &&
+pm2 start "cloud-tracker/backend/dist/index.js" --name cloud-tracker-api &&
 echo "Deploy Complete!"
