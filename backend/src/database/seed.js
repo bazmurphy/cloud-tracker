652fcb6c37450fef1c415db9c4bbeae1c871c8d0
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-10-09 13:50:20 +0100

    adjusted seed, env: connection string & ssl

--- backend/src/database/seed.js
+++ backend/src/database/seed.js
@@ -1,11 +1,8 @@
 const { Client } = require("pg");
 
 const pgclient = new Client({
-  host: process.env.POSTGRES_HOST,
-  port: process.env.POSTGRES_PORT,
-  user: process.env.POSTGRES_USER,
-  password: process.env.POSTGRES_PASSWORD,
-  database: process.env.POSTGRES_DB,
+  connectionString: process.env.DB_CONNECTION_STRING,
+  ssl: process.env.DB_SSL === "true" ? { rejectUnauthorized: false } : false,
 });
 
 async function seed() {
