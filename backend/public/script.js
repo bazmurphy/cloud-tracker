e70052be9f803ebdf636a8c6baa31e325405f8d4
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-10-11 11:54:11 +0100

    backend index adjustments

---
+++ backend/public/script.js
@@ -0,0 +1,26 @@
+const responseContainer = document.getElementById("response-container");
+const apiLinks = document.querySelectorAll(".api-link");
+
+apiLinks.forEach((apiLink) => {
+  apiLink.addEventListener("click", async (event) => {
+    event.preventDefault();
+    while (responseContainer.firstChild) {
+      responseContainer.removeChild(responseContainer.firstChild);
+    }
+    try {
+      const response = await fetch(event.target.href);
+      const data = await response.json();
+      const jsonFormatted = JSON.stringify(data, null, 2);
+      const preBlock = document.createElement("pre");
+      if (jsonFormatted.includes(`"success: true,`)) {
+        preBlock.className = "response-success";
+      } else if (jsonFormatted.includes(`"error": true,`)) {
+        preBlock.className = "response-error";
+      }
+      preBlock.innerText = jsonFormatted;
+      responseContainer.append(preBlock);
+    } catch (error) {
+      console.error(error);
+    }
+  });
+});
