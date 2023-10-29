d2879f9a7d2fc051dd3785311f4977559721d78c
Baz <61154071+bazmurphy@users.noreply.github.com>
2023-10-29 18:58:06 +0000

    adjustments

--- frontend/src/components/LoadBalancer.tsx
+++ frontend/src/components/LoadBalancer.tsx
@@ -4,23 +4,23 @@ import "./LoadBalancer.css";
 function LoadBalancer() {
   const [clientIP, setClientIP] = useState("");
 
+  const getClientIP = async () => {
+    try {
+      const response = await fetch("/api/get-client-ip");
+      const data = await response.json();
+      setClientIP(data.clientIP);
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
   useEffect(() => {
-    const getClientIP = async () => {
-      try {
-        const response = await fetch("/api/get-client-ip");
-        const data = await response.json();
-        setClientIP(data.clientIP);
-      } catch (error) {
-        console.error(error);
-      }
-    };
     getClientIP();
   }, []);
 
   return (
     <p className="load-balancer">
-      Load Balancer routed to:{" "}
-      {clientIP ? window.location.hostname : "loading..."}
+      Load Balancer routed to {clientIP ? clientIP : "[fetching...]"}
     </p>
   );
 }
