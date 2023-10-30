0cfc2d7678ae190d6636e2fbab46f769fe00a3dc
Baz Murphy <bazmurphy@gmail.com>
2023-10-30 12:30:57 +0000

    adjustments

--- frontend/src/components/LoadBalancer.tsx
+++ frontend/src/components/LoadBalancer.tsx
@@ -1,11 +1,10 @@
-// import { useState, useEffect } from "react";
 import "./LoadBalancer.css";
 
 function LoadBalancer() {
+  const ec2InternalIp = import.meta.env.VITE_EC2_INTERNAL_IP;
+  console.log("ec2InternalIp:", ec2InternalIp);
   return (
-    <p className="load-balancer">
-      Load Balancer routed to {import.meta.env.VITE_EC2_INTERNAL_IP}
-    </p>
+    <p className="load-balancer">Load Balancer routed to {ec2InternalIp}</p>
   );
 }
 
